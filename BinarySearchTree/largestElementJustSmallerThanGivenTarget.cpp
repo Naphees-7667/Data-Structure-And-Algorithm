626d38a85ef83a12c41bb625ce699cc3689aa4a1
Naphees <naphees6200@gmail.com>
2025-01-22 22:28:34 +0530

    Bst Question

---
+++ BinarySearchTree/largestElementJustSmallerThanGivenTarget.cpp
@@ -0,0 +1,21 @@
+/*
+
+Largest Smaller BST Key
+Given a root of a Binary Search Tree (BST) and a number num, implement an efficient function findLargestSmallerKey that finds the largest key in the tree that is smaller than num. If such a number doesn't exist, return -1. Assume that all keys in the tree are nonnegative.
+
+Analyze the time and space complexities of your solution.
+
+int findLargestSmallerKey(Node *rootNode, int num) 
+{
+  int res = -1;
+  while(rootNode != nullptr){
+   if(rootNode->val < num){
+      res = rootNode->val;
+      rootNode = rootNode->right;
+   }
+   else rootNode = rootNode->left;
+  }
+  return res;
+}
+
+*/
